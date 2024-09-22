b326059b5c2aeb089223386e3528cac33d0dcc48
Andrei V. Petrov <avpetrov@ut-it.ru>
2024-09-22 15:28:03 +0400

    Исправлена вёрстка карт

--- src/app/EventCard.tsx
+++ src/app/EventCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from "antd";
+import { Button, Card } from "antd";
 import Image from 'next/image'
 import { formatDate } from "./utils";
 import Link from 'next/link'
@@ -18,12 +18,17 @@ export const EventCard: React.FC<EventCardProps> = ({title, desc, imageUrl, date
     <h5>{formatDate(date)}</h5>
     <h4 style={{textWrap: 'balance'}}>{title}</h4>
         </>
-    return <Link href={href} ><Card
-      hoverable
-      style={{ width: 240, minHeight: '100%' }}
-      cover={<Image alt="АФИША" src={imageUrl} width={280} height={300}/>}
-    >
-        <CMeta title={metaTitle} description={desc} />
-    </Card>
+    return <Link href={href} >
+        <Card
+        hoverable 
+        style={{ width: 240, minHeight: '100%', display: 'flex', flexDirection: 'column'}}
+        cover={<Image alt="АФИША" src={imageUrl} width={280} height={300}/>}
+        bodyStyle={{display: 'flex', flexDirection: 'column', alignItems: 'center', flexGrow: 1}}
+        >
+            <div style={{flexGrow: '1', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}} >
+            <CMeta title={metaTitle} description={desc} />
+            <Button style={{marginTop: '1.5rem'}}>подробности</Button>
+            </div>
+        </Card>
     </Link>
 };
