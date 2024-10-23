5b0b94c0f783952ea73cebedda89179c717d3145
AndreySFN <andreysfn@yandex.ru>
2024-10-23 14:29:13 +0400

    Карта поправлена

--- src/atoms/maps/MainMap.tsx
+++ src/atoms/maps/MainMap.tsx
@@ -30,7 +30,7 @@ export const MainMap = ({ className }: { className?: string }) => (
     <iframe
       src="https://yandex.com/map-widget/v1/?ll=36.729956%2C56.329243&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1Njg2MjA3NBJf0KDQvtGB0YHQuNGPLCDQnNC-0YHQutC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwg0JrQu9C40L0sINGD0LvQuNGG0LAg0JfQsNGF0LLQsNGC0LDQtdCy0LAsIDQiCg166xJCFSVRYUI%2C&source=serp_navig&z=17.13"
       width="100%"
-      height="400"
+      height="100%"
       allowFullScreen
       style={{ position: 'relative' }}
     />
--- src/sections/AddressSection.tsx
+++ src/sections/AddressSection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import styles from './AddressSection.module.scss';
 import { MapsDictionary, TMapKeys } from '@/atoms/maps/MapsDictionary';
+import { PHONE_NUMBER } from '@/consts';
 
 export interface IAddressSectionProps {
   address?: string;
@@ -22,54 +23,44 @@ export const AddressSection: React.FC<IAddressSectionProps> = ({
   return (
     <>
       <section className={styles.address}>
-        <div
-          style={{
-            position: 'relative',
-            height: '100%',
-            width: '100%',
-            overflow: 'hidden',
-          }}
-        >
-          <div className={styles.content}>
-            <h2 style={{ fontSize: '2rem' }}>{title}</h2>
-            <ul className={styles.contactBlock}>
-              <li>
-                <strong>📍 Адрес:</strong> г. Клин, ул Захватаева 4. Вход слева
-                с торца
-              </li>
-              <li>
-                <h4>🎟️ Заказ билетов:</h4>
-                <ul className={styles.nestedList}>
-                  <li>💻 Доступен онлайн на сайте</li>
-                  <li>
-                    📱 По телефону:{' '}
-                    <a href="tel:+79067370208">+7 (906) 737 02-08</a>
-                  </li>
-                  <li style={{ fontSize: '0.8rem' }}>
-                    Если вам не удалось дозвониться, отправьте SMS со словом
-                    «ЛУНА», и мы обязательно перезвоним!
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <h4>💼 По вопросам сотрудничества</h4>
-                <ul className={styles.nestedList}>
-                  <li>
-                    📞 Телефон:{' '}
-                    <a href="tel:+79067370208">+7 (906) 737 02-08</a>
-                  </li>
-                  <li>
-                    ✉️ Email:{' '}
-                    <a href="mailto:art-director@luna-art-studio.ru">
-                      art-director@luna-art-studio.ru
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-          <Map className={styles.mapContainer} />
+        <div className={styles.content}>
+          <h2 style={{ fontSize: '2rem' }}>{title}</h2>
+          <ul className={styles.contactBlock}>
+            <li>
+              <strong>📍 Адрес:</strong> г. Клин, ул Захватаева 4. Вход слева с
+              торца
+            </li>
+            <li>
+              <h4>🎟️ Заказ билетов:</h4>
+              <ul className={styles.nestedList}>
+                <li>💻 Доступен онлайн на сайте</li>
+                <li>
+                  📱 По телефону:{' '}
+                  <a href="tel:+79067370208">+7 (906) 737 02-08</a>
+                </li>
+                <li style={{ fontSize: '0.8rem' }}>
+                  Если вам не удалось дозвониться, отправьте SMS со словом
+                  «ЛУНА», и мы обязательно перезвоним!
+                </li>
+              </ul>
+            </li>
+            <li>
+              <h4>💼 По вопросам сотрудничества</h4>
+              <ul className={styles.nestedList}>
+                <li>
+                  📞 Телефон: <a href={`tel:${PHONE_NUMBER}`}>{PHONE_NUMBER}</a>
+                </li>
+                <li>
+                  ✉️ Email:{' '}
+                  <a href="mailto:art-director@luna-art-studio.ru">
+                    art-director@luna-art-studio.ru
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
+        <Map className={styles.mapContainer} />
       </section>
     </>
   );
